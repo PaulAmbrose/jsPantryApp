9287aa196109e61dd491ddc079be546a8b2b03f5
Paul <pablo_rose34@hotmail.com>
2023-07-19 20:42:38 +0100

    update

---
+++ js_code/food_Item.js
@@ -0,0 +1,20 @@
+"use strict";
+
+class FoodItem {
+
+    constructor() {
+      let itemDetails = this.getItemDetails();
+      this.name = itemDetails[0];
+      this.metric = itemDetails[1];
+      this.amount = itemDetails[2];   
+    }
+
+    getItemDetails(){
+      var FoodItemName = prompt("Please enter the food items name: ")
+      var FoodItemMetric = prompt("Please enter the food items metric (weight, volume or number):  ")
+      var FoodItemAmount = prompt("Please enter the amount of the item to add:  ")
+      return [FoodItemName, FoodItemMetric, FoodItemAmount];
+    }
+  }
+
+  export{foodItem};
---
+++ js_code/kitchen.js
@@ -0,0 +1,89 @@
+"use strict";
+
+let kitchens = [];
+
+class Kitchen {
+
+    constructor() {
+      let kitchenName = this.getKitchenName();
+      this.Name = kitchenName;
+      this.Fridge = new Fridge();
+      this.Freezer = new Freezer();
+      this.StoreCupboards = new StoreCupboards();
+      this.Unsorted = []
+    }  
+
+    getKitchenName() {
+      var usersNameForKitchen = prompt("Please enter the kitchens name: ");
+      return usersNameForKitchen;
+    }
+
+  }
+  
+  class Fridge {
+  
+    constructor() {
+      this.contents = [];
+    }
+  
+    addItem(item) {
+      this.contents.push(item);
+    }
+  
+    removeItem(item) {
+      this.contents.splice(this.contents.indexOf(item), 1);
+    }
+  
+    listContents() {
+      console.log("The contents of the fridge are:");
+      for (const item of this.contents) {
+        console.log(item);
+      }
+    }
+  }
+  
+  class Freezer {
+  
+    constructor() {
+      this.contents = [];
+    }
+  
+    addItem(item) {
+      this.contents.push(item);
+    }
+  
+    removeItem(item) {
+      this.contents.splice(this.contents.indexOf(item), 1);
+    }
+  
+    listContents() {
+      console.log("The contents of the freezer are:");
+      for (const item of this.contents) {
+        console.log(item);
+      }
+    }
+  }
+  
+  class StoreCupboards {
+  
+    constructor() {
+      this.contents = [];
+    }
+  
+    addItem(item) {
+      this.contents.push(item);
+    }
+  
+    removeItem(item) {
+      this.contents.splice(this.contents.indexOf(item), 1);
+    }
+  
+    listContents() {
+      console.log("The contents of the store cupboards are:");
+      for (const item of this.contents) {
+        console.log(item);
+      }
+    }
+  }
+
+  export{kitchen};
