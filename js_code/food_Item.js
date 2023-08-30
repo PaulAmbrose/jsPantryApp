d0e9a154664972a92ad6ad0cf5efd7b688556678
Paul Ambrose <pablo_rose34@hotmail.com>
2023-08-30 20:17:51 +0100

    Update

--- js_code/food_Item.js
+++ js_code/food_Item.js
@@ -3,18 +3,23 @@
 class FoodItem {
 
     constructor() {
-      let itemDetails = this.getItemDetails();
+      /*let itemDetails = this.getItemDetails();
       this.name = itemDetails[0];
       this.metric = itemDetails[1];
-      this.amount = itemDetails[2];   
+      this.amount = itemDetails[2];*/   
+    
+      this.name = "TestBanana";
+      this.metric = "Count";
+      this.amount = 5;
+    
     }
 
-    getItemDetails(){
+    /*getItemDetails(){
       var FoodItemName = prompt("Please enter the food items name: ")
       var FoodItemMetric = prompt("Please enter the food items metric (weight, volume or number):  ")
       var FoodItemAmount = prompt("Please enter the amount of the item to add:  ")
       return [FoodItemName, FoodItemMetric, FoodItemAmount];
-    }
+    }*/
   }
 
   export{FoodItem};
