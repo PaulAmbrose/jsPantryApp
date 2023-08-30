8a9297c1d795b31c6a60fcc897bb3682e0cb7427
Paul Ambrose <pablo_rose34@hotmail.com>
2023-08-30 20:41:04 +0100

    update

--- js_code/add_and_remove_item.js
+++ js_code/add_and_remove_item.js
@@ -1,13 +1,14 @@
 "use strict";
 
 function addItem() {
-
+    let itemDetails = getItemDetails();
+    
 }
 
-
-    getItemDetails(){
-      var FoodItemName = prompt("Please enter the food items name: ")
-      var FoodItemMetric = prompt("Please enter the food items metric (weight, volume or number):  ")
-      var FoodItemAmount = prompt("Please enter the amount of the item to add:  ")
-      return [FoodItemName, FoodItemMetric, FoodItemAmount];
+function getItemDetails(){
+    var FoodItemName = prompt("Please enter the food items name: ")
+    var FoodItemMetric = prompt("Please enter the food items metric (weight, volume or number):  ")
+    var FoodItemAmount = prompt("Please enter the amount of the item to add:  ")
+    var FoodItemLocation= prompt("Please enter the location to add to:  ")
+      return [FoodItemName, FoodItemMetric, FoodItemAmount, FoodItemLocation];
     }
